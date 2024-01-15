d793a14a93a4b50b0716bb24f815a5f190ab0a29
yjh <860622588@qq.com>
2024-01-15 19:51:05 +0800

    feat: add NumberRange and types test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 export type PickKeysByType<T, V> = keyof {
   [K in keyof T as T[K] extends V ? K : never]: T[K];
 }
+
+type _RangeUtils<L, C extends any[] = [], R = L> =
+C['length'] extends L
+  ? R
+  : _RangeUtils<L, [...C, 0], C['length'] | R>
+
+export type NumberRange<L, H> = L | Exclude<_RangeUtils<H>, _RangeUtils<L>>
+
+export type { Expect, Equal } from './utils'
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,23 @@
-import { describe, expect, it } from 'vitest'
+/* eslint-disable ts/ban-ts-comment */
+import { describe, it } from 'vitest'
+import type { Equal, Expect, NumberRange, PickKeysByType } from '../src/index'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('test types', () => {
+  it('numberRange should working', () => {
+    type _ = Expect<Equal<NumberRange<1, 5>, 1 | 2 | 3 | 4 | 5>>
+  })
+
+  it('numberRange should error', () => {
+    // @ts-expect-error
+    type _ = Expect<Equal<NumberRange<1, 2>, 1 | 2 | 3 | 4 | 5>>
+  })
+
+  it('pickKeysByType should working', () => {
+    type _ = Expect<Equal<'name', PickKeysByType<{ name: 'john', age: 22 }, string>>>
+  })
+
+  it('pickKeysByType should error', () => {
+    // @ts-expect-error
+    type _ = Expect<Equal<'name', PickKeysByType<{ name: 'john', age: 22 }, number>>>
   })
 })
