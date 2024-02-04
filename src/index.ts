8ce021d142e468edf5f74f5d520172ae3a7e8649
yjh <860622588@qq.com>
2024-02-04 11:05:28 +0800

    feat: add PartialRequired

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+export type * from './utils'
+
+/**
+ * pick a sub object from T by the V type
+ */
 export type PickKeysByType<T, V> = keyof {
   [K in keyof T as T[K] extends V ? K : never]: T[K];
 }
@@ -7,6 +12,12 @@ C['length'] extends L
   ? R
   : _RangeUtils<L, [...C, 0], C['length'] | R>
 
+/**
+ * Generate a number range from L to H
+ */
 export type NumberRange<L, H> = L | Exclude<_RangeUtils<H>, _RangeUtils<L>>
 
-export type { Expect, Equal } from './utils'
+/**
+ * Partial required
+ */
+export type PartialRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>
